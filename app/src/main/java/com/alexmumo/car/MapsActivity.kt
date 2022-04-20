6abff14b240866e667df05cac91fb31cca27ea35
alexymumo <alexymumo2000@gmail.com>
2022-04-20 10:33:19 +0300

    first commit

---
+++ app/src/main/java/com/alexmumo/car/MapsActivity.kt
@@ -0,0 +1,43 @@
+package com.alexmumo.car
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alexmumo.car.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.* // ktlint-disable no-wildcard-imports
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    val sydney = LatLng(-1.6118365, 37.4812825)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera juja lat and long
+        // val sydney = LatLng(-1.0401167136661538, 37.12985232840806)
+
+        // Adds a marker to the coordinates
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+
+        mMap.setMinZoomPreference(6.0f)
+        mMap.setMaxZoomPreference(14f)
+
+        // centers the map at LatLng coordinates
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
---
+++ app/src/main/java/com/alexmumo/car/ui/fragment/ModalBottomSheet.kt
@@ -0,0 +1,41 @@
+package com.alexmumo.car.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.alexmumo.car.R
+import com.alexmumo.car.databinding.FragmentModalBottomSheetListDialogBinding
+import com.alexmumo.car.databinding.FragmentModalBottomSheetListDialogItemBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class ModalBottomSheet : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentModalBottomSheetListDialogBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+
+        _binding = FragmentModalBottomSheetListDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setStyle(STYLE_NORMAL, com.google.android.material.R.style.Animation_MaterialComponents_BottomSheetDialog)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
