1c85060f9dde04335282c450706a53c7f794b584
alexymumo <alexymumo2000@gmail.com>
2022-04-20 11:12:59 +0300

    added map types

--- app/src/main/java/com/alexmumo/car/MapsActivity.kt
+++ app/src/main/java/com/alexmumo/car/MapsActivity.kt
@@ -1,6 +1,8 @@
 package com.alexmumo.car
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.alexmumo.car.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.* // ktlint-disable no-wildcard-imports
@@ -36,5 +38,30 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // centers the map at LatLng coordinates
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.map_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.hybrid_map -> {
+            mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+        R.id.satellite_map -> {
+            mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+        R.id.normal_map -> {
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            true
+        }
+        R.id.terrain_map -> {
+            mMap.mapType = GoogleMap.MAP_TYPE_TERRAIN true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
