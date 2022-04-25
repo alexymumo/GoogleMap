8ac927264fcea093436ff0bfb9551b0bc9081598
alexymumo <alexymumo2000@gmail.com>
2022-04-25 10:39:36 +0300

    added splashviewmodel

--- app/src/main/java/com/alexmumo/car/ui/fragment/splash/SplashFragment.kt
+++ app/src/main/java/com/alexmumo/car/ui/fragment/splash/SplashFragment.kt
@@ -6,10 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.alexmumo.car.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashFragment : Fragment() {
+    private val viewModel: SplashViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
--- app/src/main/java/com/alexmumo/car/ui/fragment/splash/SplashViewModel.kt
+++ app/src/main/java/com/alexmumo/car/ui/fragment/splash/SplashViewModel.kt
@@ -1,5 +1,16 @@
 package com.alexmumo.car.ui.fragment.splash
 
+import android.os.Handler
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
-class SplashViewModel : ViewModel()
+
+class SplashViewModel : ViewModel() {
+    var splash: MutableLiveData<Boolean> = MutableLiveData<Boolean>()
+    fun setUp() {
+        Handler().postDelayed({
+            splash.value = FirebaseAuth.getInstance().currentUser == null
+        }, 3000)
+    }
+}
