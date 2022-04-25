2552aa676258a037c00a749377f2d9fb8808963c
alexymumo <alexymumo2000@gmail.com>
2022-04-25 11:00:24 +0300

    update

--- app/src/main/java/com/alexmumo/car/ui/fragment/splash/SplashFragment.kt
+++ app/src/main/java/com/alexmumo/car/ui/fragment/splash/SplashFragment.kt
@@ -1,7 +1,6 @@
 package com.alexmumo.car.ui.fragment.splash
 
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,20 +8,23 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.alexmumo.car.R
+import com.alexmumo.car.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashFragment : Fragment() {
+    private lateinit var binding: FragmentSplashBinding
     private val viewModel: SplashViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
-
-        Handler().postDelayed({
-            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
-        }, 3000)
+    ): View {
+        binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+        viewModel.splash.observe(viewLifecycleOwner) {
+            findNavController().navigate(R.id.action_splashFragment_to_mapsFragment)
+        }
+        viewModel.setUp()
+        return binding.root
     }
 }
