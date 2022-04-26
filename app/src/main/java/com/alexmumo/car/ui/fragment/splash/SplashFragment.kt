94b3a9b604515a619198d679887b9db264e04093
alexymumo <alexymumo2000@gmail.com>
2022-04-26 18:14:31 +0300

    splash screen

--- app/src/main/java/com/alexmumo/car/ui/fragment/splash/SplashFragment.kt
+++ app/src/main/java/com/alexmumo/car/ui/fragment/splash/SplashFragment.kt
@@ -22,7 +22,7 @@ class SplashFragment : Fragment() {
     ): View {
         binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
         viewModel.splash.observe(viewLifecycleOwner) {
-            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+            findNavController().navigate(R.id.action_splashFragment_to_mapsFragment)
         }
         viewModel.setUp()
         return binding.root
