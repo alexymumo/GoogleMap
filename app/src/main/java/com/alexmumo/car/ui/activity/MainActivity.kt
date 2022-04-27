51c480f91304846eaea4700ea159b07d0860f132
alexymumo <alexymumo2000@gmail.com>
2022-04-27 13:13:54 +0300

    navigation drawer working

--- app/src/main/java/com/alexmumo/car/ui/activity/MainActivity.kt
+++ app/src/main/java/com/alexmumo/car/ui/activity/MainActivity.kt
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.alexmumo.car.R
 import com.alexmumo.car.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +23,22 @@ class MainActivity : AppCompatActivity() {
 
         val navNavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navNavHostFragment.findNavController()
-        binding.bottomNav.setupWithNavController(navController)
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.registerFragment,
+                R.id.loginFragment,
+                R.id.homeFragment
+            ), binding.drawer
+        )
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawer)
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
+    }
+    // When up button is pressed
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            navController,
+            appBarConfiguration
+        ) || super.onSupportNavigateUp()
     }
 }
