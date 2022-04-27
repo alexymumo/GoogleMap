b686f17a40b2eaac7ce0167043c01f042e8641eb
alexymumo <alexymumo2000@gmail.com>
2022-04-27 10:48:22 +0300

    added icons

---
+++ app/src/main/java/com/alexmumo/car/ui/fragment/main/SettingsFragment.kt
@@ -0,0 +1,20 @@
+package com.alexmumo.car.ui.fragment.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.alexmumo.car.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentSettingsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
