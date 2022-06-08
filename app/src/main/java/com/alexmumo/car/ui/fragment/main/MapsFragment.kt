1d4de01aff009544edb8e93cb8c277fcaf0097dc
alexymumo <alexymumo2000@gmail.com>
2022-06-08 11:36:44 +0300

    marker

--- app/src/main/java/com/alexmumo/car/ui/fragment/main/MapsFragment.kt
+++ app/src/main/java/com/alexmumo/car/ui/fragment/main/MapsFragment.kt
@@ -1,60 +1,32 @@
 package com.alexmumo.car.ui.fragment.main
 
-import android.Manifest.permission.ACCESS_COARSE_LOCATION
-import android.Manifest.permission.ACCESS_FINE_LOCATION
-import android.content.pm.PackageManager
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.alexmumo.car.R
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.* // ktlint-disable no-wildcard-imports
-import dagger.hilt.android.AndroidEntryPoint
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-// ktlint-disable no-wildcard-imports
+class MapsFragment : Fragment() {
 
-// ktlint-disable no-wildcard-imports
-@AndroidEntryPoint
-class MapsFragment :
-    Fragment(),
-    GoogleMap.OnPolylineClickListener,
-    GoogleMap.OnPolygonClickListener {
-    private lateinit var map: GoogleMap
-    private val LOCATION = 1
     private val callback = OnMapReadyCallback { googleMap ->
-        map = googleMap
-
-        val latitude = -1.1353041
-        val longitude = 36.9443908
-        val zoomLevel = 15f
-        val polyline = googleMap.addPolyline(
-            PolylineOptions()
-                .color(Color.GREEN)
-                .width(5f)
-                .clickable(true)
-                .add(
-                    LatLng(-1.1353041, 36.9443908),
-                    LatLng(-1.3028618, 36.7069651),
-                    LatLng(-1.5128474, 37.2369496),
-                    LatLng(-1.2711339, 37.3028753)
-                ),
-        )
-        polyline.tag = "A"
-        polyline.endCap = RoundCap()
-        polyline.jointType = JointType.DEFAULT
-        val homeLatLng = LatLng(latitude, longitude)
-        googleMap.addMarker(MarkerOptions().position(homeLatLng))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
-
-        enableLocation()
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val sydney = LatLng(-0.3977286, 36.9575167)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Dekut"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     override fun onCreateView(
@@ -69,57 +41,5 @@ class MapsFragment :
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
-    }
-
-    private fun checkPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            requireContext(), ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun enableLocation() {
-        if (checkPermission()) {
-            if (ActivityCompat.checkSelfPermission(
-                    requireContext(),
-                    ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        requireContext(),
-                        ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                return
-            }
-            map.isMyLocationEnabled = true
-        } else {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(
-                    ACCESS_COARSE_LOCATION,
-                    ACCESS_FINE_LOCATION
-                ),
-                LOCATION
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == LOCATION) {
-            if (grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                enableLocation()
-            }
-        }
-    }
-
-    override fun onPolylineClick(p0: Polyline) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onPolygonClick(p0: Polygon) {
-        TODO("Not yet implemented")
     }
 }
